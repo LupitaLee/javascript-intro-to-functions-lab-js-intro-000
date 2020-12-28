a18f502aece24b1df4a2ccdb6838b4b93ffc7649
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-28 22:27:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ function shout(string) {return string.toUpperCase()
 'HELLO!'.toLowerCase() // 'hello'
 function whisper(string){return string.toLowerCase()
 }
+'i must have logged',1000, 'times today'.toUpperCase()
+function logShout(string){console.log('i must have logged',1000, 'times today')}
