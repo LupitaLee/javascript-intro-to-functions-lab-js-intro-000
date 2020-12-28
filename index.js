5e40996f492262b0e7be4300becd3ca18559ad14
Lupita Rivera <lupitarivera8899@gmail.com>
2020-12-28 22:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 'Hello!'.toUpperCase() // 'HELLO!'
 function shout(string) {return string.toUpperCase()
 }
+}
 'HELLO!'.toLowerCase() // 'hello'
 function whisper(string){return string.toLowerCase()
 }
+}
+
+function logShout (hello) {
+  console.log(string.toUpperCase( ));
